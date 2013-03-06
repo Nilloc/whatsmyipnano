3cf1d4f3e225d36c3125cf4cd1cfcdb570925d15
Collin Reisdorf <collin@paperclipped.com>
2013-03-06 11:59:17 -0500

    getting it actually ready for heroku
    added haml to bundle
    
    messing with sasquatch

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ set :haml, { :format => :html5 }
 
 
 get '/' do
-  @ip = request.env['REMOTE_ADDR'].split(',').first
+  @ip = request.env['HTTP_X_FORWARDED_FOR'] #.split(',').first
+  puts request.env
   haml :index
 end
